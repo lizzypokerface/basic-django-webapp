3fb012dd8533a2ad3922a1a814e15cd39760e6f2
glennlum <glennlum_@hotmail.com>
2024-05-29 21:46:59 +0800

    feat(playground): render response with template

--- playground/urls.py
+++ playground/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 
-# URLConf - playground/hello/
-urlpatterns = [path("hello/", views.say_hello)]  # say_hello is a function reference
+# URLConf
+urlpatterns = [path("hello/", views.say_hello)]  # playground/hello/
--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 # NOTE: A Django view is a request handler that takes a web request and returns a web response.
-# from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.shortcuts import render
+
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def say_hello(request):
-    return HttpResponse("Hello World")
+    return render(request, "hello.html", {"name": "Glenn"})
